82fe8d6e3bdd285f48c71ab910d2f19ebd1fc116
Ольга Петрова <luchezarnaya79olga@yandex.ru>
2022-11-05 14:02:11 +0300

    Изменен PlayerTest

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -20,4 +20,56 @@ public class PlayerTest {
     }
 
     // другие ваши тесты
-}
+}package ru.netology;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void shouldSumGenreIfOneGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+
+        int expected = 3;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    // другие ваши тесты
+
+    @Test
+    public void shouldSumGenreALotOfGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Змейка", "Аркады");
+
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+
+        player.play(game, 3);
+        player.play(game1, 3);
+
+
+        int expected = 6;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void PlayPlayerIfOneGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+
